6c52b13c5a7725e9b3f1596fad8e12388f9efb2b
Tomas Garcia <tgarsa@me.com>
2023-08-19 00:10:15 +0200

    Moved the utils folder

---
+++ product-api/utils/__init__.py
@@ -0,0 +1,7 @@
+
+def _get_ip():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    # print(s.getsockname()[0])
+    return s.getsockname()[0]
