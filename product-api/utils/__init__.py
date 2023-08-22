7c52d32a1be04e55ae11ebe94a606fc3f3dad26c
Tomas Garcia Saiz <tgarsa@me.com>
2023-08-22 11:10:28 +0200

    Ponemos de nuevo a mano la IP. Debemos de seguir revisando esto.

--- product-api/utils/__init__.py
+++ product-api/utils/__init__.py
@@ -1,7 +1,8 @@
 
 def _get_ip():
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
+    #import socket
+    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #s.connect(("8.8.8.8", 80))
     # print(s.getsockname()[0])
-    return s.getsockname()[0]
+    #return s.getsockname()[0]
+    return "192.168.178.189"
